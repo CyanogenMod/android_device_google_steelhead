b3734e6cf3a0493a6c235b79c0887c5afd459dad
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-01-14 20:17:06 +0000

    Make the lights test program slightly more useful
    Now accepting multiple arguments to set a multicolored ring

--- liblights/lightstest.c
+++ liblights/lightstest.c
@@ -33,32 +33,57 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if (argc>1) {
+    if (argc==2) {
         unsigned int rgb = atoi(argv[1]);
         struct avr_led_rgb_vals reg = prepare_leds(rgb);
         int val = 1;
 
-        printf("Setting LEDs to %x\n",rgb);
+        printf("Setting all LEDs to %x\n",rgb);
         printf("Component breakdown is %x/%x/%x\n",reg.rgb[0],reg.rgb[1],reg.rgb[2]);
+        ioctl(fd,AVR_LED_SET_MUTE,&reg);
         res = ioctl(fd,AVR_LED_SET_ALL_VALS,&reg);
+    } else if (argc>2) {
+        unsigned int ledcount = argc-1;
+        unsigned int rgb;
+        unsigned int colorstrip = value;
+        unsigned int padding = 0;
+        unsigned int val = 0;
+        unsigned int i = 0;
+        struct avr_led_rgb_vals color;
+        struct avr_led_set_range_vals *reg;
+
+        reg = malloc(sizeof(reg));
+
+        /* Limit to 8 colors */
+        if (ledcount > 8) ledcount = 8;
+        colorstrip = (unsigned int)(value / ledcount);
+        padding = value - (ledcount * colorstrip);
+
+        /* Set the mute LED to the first listed color */
+        color = prepare_leds(atoi(argv[1]));
+        ioctl(fd,AVR_LED_SET_MUTE,&color);
+
+        for (val=0; val < ledcount; val++) {
+            rgb = (atoi(argv[val+1]));
+            color = prepare_leds(rgb);
+            for (i=0; i<colorstrip; i++) {
+                //printf("Setting %d to 0x%.6x\n",((val*colorstrip)+i),rgb);
+                reg->rgb_vals[(val*colorstrip)+i] = color;
+            }
+        }
+        for (val=padding; val > 0; val--) {
+            reg->rgb_vals[value-val] = color;
+            //printf("Setting %d to 0x%.6x\n",value-val,rgb);
+        }
+
+        reg->start = 0;
+        reg->count = value;
+        reg->rgb_triples = value;
+        res = ioctl(fd,AVR_LED_SET_RANGE_VALS,reg);
+        free(reg);
     }
 
-    /*if (argc>1) {
-        unsigned int rgb = atoi(argv[1]);
-        struct avr_led_rgb_vals color = prepare_leds(rgb);
-        struct avr_led_set_range_vals reg;
-        int val = 1;
-
-        reg.start = 0;
-        reg.count = 20;
-        reg.rgb_triples = 2;
-        reg.rgb_vals[0] = color;
-        reg.rgb_vals[1] = prepare_leds(8388608);
-        res = ioctl(fd,AVR_LED_SET_RANGE_VALS,&reg);
-    }*/
-
     close(fd);
-    return ( res ? -1 : 0 );
-
+    return (res ? 1 : 0);
 }
 
